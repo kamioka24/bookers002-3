220c4407ce4e4c8aa8382553889bf8fd4e1c124a
kamioka24 <keee2404@gmail.com>
2020-05-25 11:26:27 +0900

    [Finish]

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,6 +2,7 @@ class SearchController < ApplicationController
 	def search
 		@user_or_book = params[:option]
 		@how_search = params[:choice]
+		@word = params[:search] #searchした言葉をViewに呼び出すための定義
 		if @user_or_book == "1"
 		   @users = User.search(params[:search], @user_or_book, @how_search)
 		else
